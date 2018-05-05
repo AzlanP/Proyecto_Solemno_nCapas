cfa20543fc026411781388e3c7d37b6d652126ce
AzlanP <pablo.1994.azlan@gmail.com>
2018-05-05 00:57:14 -0300

    Inicial
    Solo tiene las librerias de clases creada para cada capa, y se creo la clase conexion importando la libreria sqlite

---
+++ GraficaSolemno/CapaDatos/Conexion.vb
@@ -0,0 +1,6 @@
+﻿'importo la libreria de sqlite
+' si una libreria no aparece en el imports significa que debes agregar la referencia a la misma
+Imports System.Data.SQLite
+Public Class Conexion
+
+End Class
