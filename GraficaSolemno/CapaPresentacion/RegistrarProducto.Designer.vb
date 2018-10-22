7806aad3876f7466e6f1eba31a50bdfb742809f2
AzlanP <pablo.1994.azlan@gmail.com>
2018-10-22 16:55:40 -0300

    formulario de registrar producto

---
+++ GraficaSolemno/CapaPresentacion/RegistrarProducto.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RegistrarProducto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnRegistrarProducto = New System.Windows.Forms.Button()
+        Me.lblCodigo = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.lblDescripcion = New System.Windows.Forms.Label()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.lblTipo = New System.Windows.Forms.Label()
+        Me.txtTipo = New System.Windows.Forms.TextBox()
+        Me.lblPrecio = New System.Windows.Forms.Label()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.lblCantidad = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnGuardarProducto = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(38, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 18)
+        Me.Label1.TabIndex = 34
+        Me.Label1.Text = " ID :"
+        '
+        'btnRegistrarProducto
+        '
+        Me.btnRegistrarProducto.Location = New System.Drawing.Point(41, 254)
+        Me.btnRegistrarProducto.Name = "btnRegistrarProducto"
+        Me.btnRegistrarProducto.Size = New System.Drawing.Size(75, 23)
+        Me.btnRegistrarProducto.TabIndex = 33
+        Me.btnRegistrarProducto.Text = "Registrar Producto"
+        Me.btnRegistrarProducto.UseVisualStyleBackColor = True
+        '
+        'lblCodigo
+        '
+        Me.lblCodigo.AutoSize = True
+        Me.lblCodigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCodigo.Location = New System.Drawing.Point(38, 200)
+        Me.lblCodigo.Name = "lblCodigo"
+        Me.lblCodigo.Size = New System.Drawing.Size(60, 18)
+        Me.lblCodigo.TabIndex = 32
+        Me.lblCodigo.Text = "Codigo "
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCodigo.Location = New System.Drawing.Point(148, 200)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(100, 24)
+        Me.txtCodigo.TabIndex = 31
+        '
+        'lblDescripcion
+        '
+        Me.lblDescripcion.AutoSize = True
+        Me.lblDescripcion.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescripcion.Location = New System.Drawing.Point(38, 168)
+        Me.lblDescripcion.Name = "lblDescripcion"
+        Me.lblDescripcion.Size = New System.Drawing.Size(87, 18)
+        Me.lblDescripcion.TabIndex = 30
+        Me.lblDescripcion.Text = "Descripcion"
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDescripcion.Location = New System.Drawing.Point(148, 167)
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.Size = New System.Drawing.Size(100, 24)
+        Me.txtDescripcion.TabIndex = 29
+        '
+        'lblTipo
+        '
+        Me.lblTipo.AutoSize = True
+        Me.lblTipo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTipo.Location = New System.Drawing.Point(38, 136)
+        Me.lblTipo.Name = "lblTipo"
+        Me.lblTipo.Size = New System.Drawing.Size(37, 18)
+        Me.lblTipo.TabIndex = 28
+        Me.lblTipo.Text = "Tipo"
+        '
+        'txtTipo
+        '
+        Me.txtTipo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTipo.Location = New System.Drawing.Point(148, 134)
+        Me.txtTipo.Name = "txtTipo"
+        Me.txtTipo.Size = New System.Drawing.Size(100, 24)
+        Me.txtTipo.TabIndex = 27
+        '
+        'lblPrecio
+        '
+        Me.lblPrecio.AutoSize = True
+        Me.lblPrecio.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrecio.Location = New System.Drawing.Point(38, 104)
+        Me.lblPrecio.Name = "lblPrecio"
+        Me.lblPrecio.Size = New System.Drawing.Size(51, 18)
+        Me.lblPrecio.TabIndex = 26
+        Me.lblPrecio.Text = "Precio"
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrecio.Location = New System.Drawing.Point(148, 101)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 24)
+        Me.txtPrecio.TabIndex = 25
+        '
+        'lblCantidad
+        '
+        Me.lblCantidad.AutoSize = True
+        Me.lblCantidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCantidad.Location = New System.Drawing.Point(38, 72)
+        Me.lblCantidad.Name = "lblCantidad"
+        Me.lblCantidad.Size = New System.Drawing.Size(66, 18)
+        Me.lblCantidad.TabIndex = 24
+        Me.lblCantidad.Text = "Cantidad"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCantidad.Location = New System.Drawing.Point(148, 68)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 24)
+        Me.txtCantidad.TabIndex = 23
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNombre.Location = New System.Drawing.Point(38, 40)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(62, 18)
+        Me.lblNombre.TabIndex = 22
+        Me.lblNombre.Text = "Nombre"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombre.Location = New System.Drawing.Point(148, 35)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 24)
+        Me.txtNombre.TabIndex = 21
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblID.Location = New System.Drawing.Point(148, 8)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(12, 18)
+        Me.lblID.TabIndex = 20
+        Me.lblID.Text = " "
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(148, 254)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 19
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'btnGuardarProducto
+        '
+        Me.btnGuardarProducto.Location = New System.Drawing.Point(41, 254)
+        Me.btnGuardarProducto.Name = "btnGuardarProducto"
+        Me.btnGuardarProducto.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardarProducto.TabIndex = 18
+        Me.btnGuardarProducto.Text = "Guardar"
+        Me.btnGuardarProducto.UseVisualStyleBackColor = True
+        '
+        'RegistrarProducto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 284)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRegistrarProducto)
+        Me.Controls.Add(Me.lblCodigo)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.lblDescripcion)
+        Me.Controls.Add(Me.txtDescripcion)
+        Me.Controls.Add(Me.lblTipo)
+        Me.Controls.Add(Me.txtTipo)
+        Me.Controls.Add(Me.lblPrecio)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.lblCantidad)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnGuardarProducto)
+        Me.Name = "RegistrarProducto"
+        Me.Text = "RegistrarProducto"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnRegistrarProducto As System.Windows.Forms.Button
+    Friend WithEvents lblCodigo As System.Windows.Forms.Label
+    Friend WithEvents txtCodigo As System.Windows.Forms.TextBox
+    Friend WithEvents lblDescripcion As System.Windows.Forms.Label
+    Friend WithEvents txtDescripcion As System.Windows.Forms.TextBox
+    Friend WithEvents lblTipo As System.Windows.Forms.Label
+    Friend WithEvents txtTipo As System.Windows.Forms.TextBox
+    Friend WithEvents lblPrecio As System.Windows.Forms.Label
+    Friend WithEvents txtPrecio As System.Windows.Forms.TextBox
+    Friend WithEvents lblCantidad As System.Windows.Forms.Label
+    Friend WithEvents txtCantidad As System.Windows.Forms.TextBox
+    Friend WithEvents lblNombre As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents btnGuardarProducto As System.Windows.Forms.Button
+End Class
---
+++ GraficaSolemno/CapaPresentacion/RegistrarProducto.vb
@@ -0,0 +1,71 @@
+﻿Imports CapaEntidad
+Imports CapaNegocio
+Public Class RegistrarProducto
+    Dim oCEProducto As New CEProducto
+    Dim oCNProducto As New CNProducto
+
+
+    Public Function TomarDatos() As CEProducto
+
+        oCEProducto.IDProductos = CInt(lblID.Text)
+        oCEProducto.Nombre = txtNombre.Text
+        oCEProducto.Cantidad = CInt(txtCantidad.Text)
+        oCEProducto.Precio = CDbl(txtPrecio.Text)
+        oCEProducto.Tipo = txtTipo.Text
+        oCEProducto.Descripcion = txtDescripcion.Text
+        oCEProducto.Codigo = CInt(txtCodigo.Text)
+        Return oCEProducto
+
+    End Function
+    Public Sub LlenarFormulario(ByVal ID As Integer)
+        Dim dt As New DataTable
+        Dim dr As DataRow
+        dt = oCNProducto.BuscarProducto("IDProductos", ID)
+        dr = dt.Rows(0)
+        lblID.Text = dr("IDProductos").ToString
+        txtNombre.Text = dr("Nombre").ToString
+        txtCantidad.Text = dr("Cantidad").ToString
+        txtPrecio.Text = dr("Precio").ToString
+        txtTipo.Text = dr("Tipo").ToString
+        txtDescripcion.Text = dr("Descripcion").ToString
+        txtCodigo.Text = dr("Codigo").ToString
+
+        btnRegistrarProducto.Visible = False
+        btnGuardarProducto.Visible = True
+
+        Me.Text = "Modificar Producto"
+    End Sub
+    Private Sub btnGuardar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGuardarProducto.Click
+        oCNProducto.ModificarProducto(TomarDatos())
+        MsgBox("Los Datos fueron modificados con exito.")
+        Close()
+    End Sub
+
+    Private Sub btnCancelar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.Click
+        If MessageBox.Show("Esta seguro de descartar los cambios?", "Confirmacion!", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) = DialogResult.OK Then
+            Close()
+        End If
+
+    End Sub
+    Public Sub Disesabletext()
+        'este codigo es para desabilitar la edicion de todos los campos
+        Dim ctrl As Control
+
+        For Each ctrl In Controls
+            If TypeOf ctrl Is TextBox Or TypeOf ctrl Is ComboBox Or TypeOf ctrl Is DateTimePicker Then
+                ctrl.Enabled = False 'Creo que el error es aqui
+            End If
+        Next
+
+    End Sub
+
+    Private Sub btnRegistrarProducto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRegistrarProducto.Click
+        oCNProducto.RegistrarProducto(TomarDatos())
+        MsgBox("Los Datos fueron guardados con exito.")
+        Close()
+    End Sub
+
+    Private Sub RegistrarProducto_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
